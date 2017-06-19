11c709d2e7a576b070a9c20459e9370d14e3273f
Haoran.Wang <elven.wang@nxp.com>
2017-06-19 17:14:32 +0800

    chipidea: imx: add usb vbus power polarity setting
    Cherry-picked from 8cc3298159f6330f1e843644b119f16e5059d15f.
    If the vbus is controlled by ehci port power bit, we need set power
    polarity of vbus enable signal according to the vbus power
    supply chip on board.
    
    Change-Id: I63ee496f6bd14470a2607876a09ba4a7e8b97b20
    Signed-off-by: Haoran.Wang <elven.wang@nxp.com>

--- drivers/usb/chipidea/ci_hdrc_imx.c
+++ drivers/usb/chipidea/ci_hdrc_imx.c
@@ -196,6 +196,9 @@ static struct imx_usbmisc_data *usbmisc_get_init_data(struct device *dev)
 	if (of_find_property(np, "disable-over-current", NULL))
 		data->disable_oc = 1;
 
+	if (of_find_property(np, "power-polarity-active-high", NULL))
+	            data->pwr_polarity = 1;
+
 	if (of_find_property(np, "external-vbus-divider", NULL))
 		data->evdo = 1;
 
